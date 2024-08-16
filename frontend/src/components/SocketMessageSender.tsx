fbc39ea26af8d5f5f3ffa16b6a091109a6be25f8
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2024-08-16 22:07:45 +0500

    fix: handle input properly

--- frontend/src/components/SocketMessageSender.tsx
+++ frontend/src/components/SocketMessageSender.tsx
@@ -1,3 +1,4 @@
+import React, { useState , useRef, useEffect} from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -5,6 +6,36 @@ import useResponsiveValue from './useResponsiveScreen';
 
 export const SocketMessageSender = () => {
   const screenSize: string = useResponsiveValue();
+  const [message, setMessage] = useState("");
+  const inputRef  = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus(); // Focus input on mount
+    }
+  }, []);
+
+  function onSendButtonClick(e: React.MouseEvent | React.KeyboardEvent) {
+    e.preventDefault();
+    console.log("Send button clicked");
+    console.log("Message: ", message);
+    // Reset the message
+    setMessage("");
+
+    if(inputRef.current){
+      inputRef.current.focus(); // Focus input after sending message
+    }
+  }
+
+  function handleMessageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setMessage(e.target.value);
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent) {
+    if (e.key === "Enter") {
+      onSendButtonClick(e);
+    }
+  }
 
   return (
     <Stack
@@ -18,13 +49,19 @@ export const SocketMessageSender = () => {
         variant='standard'
         id="outlined-search"
         label="Message"
+        fullWidth
         type="text"
-        style={{ flex: 1 }}
+        value={message}
+        onChange={handleMessageChange}
+        onKeyDown={handleKeyDown}
+        inputRef={inputRef} // Attach ref to TextField
       />
 
       <Button
         variant='contained'
         style={{ padding: "0 30px", minHeight: 30 }}
+        onClick={onSendButtonClick}
+        disabled={message.trim() === ""} // Disable button if message is empty
       >
         Send
       </Button>
