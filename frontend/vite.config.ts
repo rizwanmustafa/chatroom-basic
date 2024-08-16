f8d84a54b0b69ad5cf600190e3c1696fd4cb79ea
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2024-08-16 20:32:48 +0500

    chore: modify vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // To allow for easier testing on mobile devices
+    host: true,
+    port: 3000,
+  },
   plugins: [react()],
 })
